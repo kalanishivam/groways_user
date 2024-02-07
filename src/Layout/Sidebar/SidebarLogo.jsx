9a5b93b4f0b3b8fc0241d48ba4f810ca92c80cee
harsh <harshguptag1999@gmail.com>
2024-02-07 15:12:38 +0530

    for-trial

--- src/Layout/Sidebar/SidebarLogo.jsx
+++ src/Layout/Sidebar/SidebarLogo.jsx
@@ -1,13 +1,14 @@
-import React, { useContext, useState } from 'react';
-import { Grid } from 'react-feather';
-import { Link } from 'react-router-dom';
-import { Image } from '../../AbstractElements';
-import CubaIcon from '../../assets/images/logo/logo.png';
-import CustomizerContext from '../../_helper/Customizer';
-import logo from './images/GrowWaysLogo.png'
+import React, { useContext, useState } from "react";
+import { Grid } from "react-feather";
+import { Link } from "react-router-dom";
+import { Image } from "../../AbstractElements";
+import CubaIcon from "../../assets/images/logo/logo.png";
+import CustomizerContext from "../../_helper/Customizer";
+import logo from "./images/GrowWaysLogo.png";
 
 const SidebarLogo = () => {
-  const { mixLayout, toggleSidebar, toggleIcon, layout, layoutURL } = useContext(CustomizerContext);
+  const { mixLayout, toggleSidebar, toggleIcon, layout, layoutURL } =
+    useContext(CustomizerContext);
 
   const openCloseSidebar = () => {
     toggleSidebar(!toggleIcon);
@@ -16,24 +17,36 @@ const SidebarLogo = () => {
   const layout1 = localStorage.getItem("sidebar_layout") || layout;
 
   return (
-    <div className='logo-wrapper'>
-      {layout1 !== 'compact-wrapper dark-sidebar' && layout1 !== 'compact-wrapper color-sidebar' && mixLayout ? (
+    <div className="logo-wrapper">
+      {layout1 !== "compact-wrapper dark-sidebar" &&
+      layout1 !== "compact-wrapper color-sidebar" &&
+      mixLayout ? (
         <Link to={`/dashboard/default/${layoutURL}`}>
-          <div style={{display : "flex" , alignItems : "center"}}>
-          <Image attrImage={{ className: 'img-fluid d-inline', src: logo, alt: '' , style : {height : "90px" , width : "80px"}}} />
-            <p style={{fontSize : "20px" , marginTop : "20px"}}>Groways</p>
-            </div>
+          <div style={{ display: "flex", alignItems: "center" }}>
+            <Image
+              attrImage={{
+                className: "img-fluid d-inline",
+                src: logo,
+                alt: "",
+                style: { height: "90px", width: "80px" },
+              }}
+            />
+            <p style={{ fontSize: "20px", marginTop: "20px" }}>Groways</p>
+          </div>
         </Link>
       ) : (
         <Link to={`/dashboard/default/${layoutURL}`}>
-          <Image attrImage={{ className: 'img-fluid d-inline', src: `${require('../../assets/images/logo/logo_dark.png')}`, alt: '' }} />
+          <p style={{ fontSize: "22px", lineHeight: "0", color: "white",paddingTop:"3px",marginTop:"8px" }}>
+            Groways
+          </p>
+          {/* <Image attrImage={{ className: 'img-fluid d-inline', src: `${require('../../assets/images/logo/logo_dark.png')}`, alt: '' }} /> */}
         </Link>
       )}
-      <div className='back-btn' onClick={() => openCloseSidebar()}>
-        <i className='fa fa-angle-left'></i>
+      <div className="back-btn" onClick={() => openCloseSidebar()}>
+        <i className="fa fa-angle-left"></i>
       </div>
-      <div className='toggle-sidebar' onClick={openCloseSidebar}>
-        <Grid className='status_toggle middle sidebar-toggle' />
+      <div className="toggle-sidebar" onClick={openCloseSidebar}>
+        <Grid className="status_toggle middle sidebar-toggle" />
       </div>
     </div>
   );
